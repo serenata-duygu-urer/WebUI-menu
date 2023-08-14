ec8820b9a3d2305bf5598584b8b1dba97a3317c3
Duygu Ürer <45100700+duygugungor@users.noreply.github.com>
2023-08-14 11:58:26 +0200

    menu updated to sticky, dropdown css improved

--- main.js
+++ main.js
@@ -1,91 +1,63 @@
 (function () {
-  var menuTrigger = function (trigger, details, issub, maindetails) {
+  var mobileMenuTrigger = function (trigger, details) {
     trigger.addEventListener('click', () => {
       /** Slide down. */
-      if (!details.classList.contains('active')) {
+      if (!details.classList.contains('open')) {
         /** Show the menuDetails. */
-        details.classList.add('active')
-        details.style.height = "auto"
-        var mainMenuHeight = 0;
-        if (issub) {
-          mainMenuHeight = maindetails.clientHeight;
-        }
-        /** Get the computed height of the container. */
-        var height = details.clientHeight;
-        
-        /** Set the height of the content as 0px, */
-        /** so we can trigger the slide down animation. */
-        details.style.height = "0px";
-        if (issub) {
-          maindetails.style.height = maindetails.clientHeight + 'px';
-        }
-        /** Do this after the 0px has applied. */
-        /** It's like a delay or something. MAGIC! */
-        setTimeout(() => {
-          details.style.height = height + "px";
-          if (issub) {
-            maindetails.style.height = (height + mainMenuHeight) + "px";
-          }
-        }, 0);
-        /** Slide up. */
+        details.classList.add('open');
+        details.classList.remove('close')
       } else {
-        /** Set the height as 0px to trigger the slide up animation. */
-        var height = details.clientHeight;
-        details.style.height = "0px"
-        if (issub) {
-          maindetails.style.height = (maindetails.clientHeight - height) + 'px';
-        }
-        /** Remove the `active` class when the animation ends. */
-        details.addEventListener('transitionend', () => {
-          details.classList.remove('active')
-        }, { once: true })
+        details.classList.add('close');
+        details.classList.remove('open');
       }
     });
   };
-
   var setMenu = function (menuDetails, menuFlowersDetails, menuPlantDetails) {
     /** Main menu**/
     var menuBars = document.getElementById('menu-bars');
     if (menuDetails && menuBars) {
-      menuTrigger(menuBars, menuDetails, false, null);
+      mobileMenuTrigger(menuBars, menuDetails, false, null);
     }
     /** Flower sub menu**/
     var menuFlowersTrigger = document.getElementById('menu-flowers');
     if (menuFlowersTrigger && menuFlowersDetails) {
-      menuTrigger(menuFlowersTrigger, menuFlowersDetails, true, menuDetails);
+      mobileMenuTrigger(menuFlowersTrigger, menuFlowersDetails, true, menuDetails);
     }
     /** Plant sub menu**/
     var menuPlantTrigger = document.getElementById('menu-plants');
     if (menuPlantDetails && menuPlantTrigger) {
-      menuTrigger(menuPlantTrigger, menuPlantDetails, true, menuDetails);
+      mobileMenuTrigger(menuPlantTrigger, menuPlantDetails, true, menuDetails);
     }
   };
 
-  var setResizeMenu = function (element) {
-    element.removeAttribute('style');
-    element.style.height = element.clientHeight + 'px';
-  };
-
-  var resizeMenu = function (menuDetails, menuFlowersDetails, menuPlantDetails) {
-    if (menuDetails.classList.contains('active')) {
-      if (menuFlowersDetails.classList.contains('active')) {
-        setResizeMenu(menuFlowersDetails);
-      }
-      if (menuPlantDetails.classList.contains('active')) {
-        setResizeMenu(menuPlantDetails);
-      }
-      setResizeMenu(menuDetails);
-    }
-  };
+  var lastScroll = 0;
 
   document.addEventListener("DOMContentLoaded", function (event) {
+    var menuMain = document.getElementById('menu-main');
     var menuDetails = document.getElementById('menu-details');
     var menuFlowersDetails = document.getElementById('submenu-flowers');
     var menuPlantDetails = document.getElementById('submenu-plants');
     setMenu(menuDetails, menuFlowersDetails, menuPlantDetails);
-    window.addEventListener('resize', function () {
-      console.log('resize');
-      resizeMenu(menuDetails, menuFlowersDetails, menuPlantDetails);
-    }, true);
+    window.addEventListener("scroll", () => {
+      var currentScroll = window.scrollY;
+      if (currentScroll - lastScroll > menuDetails.clientHeight) {
+        // scrolled down -- header hide
+        menuMain.classList.add("close");
+        menuMain.classList.remove("open");
+        if (menuDetails.classList.contains('open')) {
+          menuDetails.classList.add('close');
+          menuDetails.classList.remove('open')
+        }
+      } else {
+        // scrolled up -- header show
+        menuMain.classList.add("open");
+        menuMain.classList.remove("close");
+        if (menuDetails.classList.contains('open')) {
+          menuDetails.classList.add('close');
+          menuDetails.classList.remove('open');
+        }
+      }
+      lastScroll = currentScroll;
+    });
   });
 })();
